f62d1e7a1aeb7913475377d832edf12d1adcf0d8
Siphosenkosi Ndhlovu <sipho.ndhlovu@outlook.com>
2021-04-22 10:43:44 +0200

    Added media component

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -1,2 +1,15 @@
 //eslint
-import { Dropdown, Accordion, Modal } from 'bootstrap';
+import { Dropdown, Accordion, Modal, Collapse } from 'bootstrap';
+
+
+const layaan = document.querySelector('#layaan');
+const lionCollapsible = new Collapse(layaan, {toggle: false});
+
+const lion = document.querySelector('.lion');
+let isMouseOver =    false
+lion.addEventListener('mouseenter', function(e){
+  lionCollapsible.hide()
+})
+lion.addEventListener('mouseleave', function(e){
+  lionCollapsible.show()
+})
